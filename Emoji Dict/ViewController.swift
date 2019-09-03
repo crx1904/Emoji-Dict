1004b5729c0e9a4a4d9c823d73ab0940be64c0cf
Mark Heitfeldt <cruxis0201@yahoo.de>
2019-09-03 21:27:11 +0200

    Initial Commit

--- Emoji Dict/ViewController.swift
+++ Emoji Dict/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
 
